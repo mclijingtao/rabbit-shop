423fec4f9a014c4b8e21f5964bffa3f33edbec49
fate <2914929295@qq.com>
2023-05-30 22:16:41 +0800

    add category

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "Layout",
       component: () => import("@/views/Layout/LayoutIndex.vue"),
       children: [
         {
           path: "",
-          name: "home",
           component: () => import("@/views/Home/HomeIndex.vue"),
         },
         {
-          path: "category",
-          name: "category",
-          component: () => import("@/views/Category/CategoryIndex.vue")
+          path: "category/:id",
+          component: () => import("@/views/Category/CategoryIndex.vue"),
         },
       ],
     },
     {
       path: "/login",
-      name: "login",
       component: () => import("@/views/Login/LoginIndex.vue"),
     },
   ],
 });
 
-export default router
+export default router;
--- src/views/Layout/LayoutIndex.vue
+++ src/views/Layout/LayoutIndex.vue
@@ -3,7 +3,7 @@ import { onMounted } from "vue"
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
-import LayoutFixed from './components/LayoutFixed.vue';
+import LayoutFixed from './components/LayoutFixed.vue'
 
 import { useCategoryStore } from '@/stores/category'
 const categoryStore = useCategoryStore()
@@ -20,6 +20,4 @@ onMounted(() => {
     <LayoutFooter />
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
